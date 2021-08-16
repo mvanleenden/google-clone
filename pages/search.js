e72a84410f4c99d9c7d4d403e0b57b7a956761b2
Mayron van Leenden <mvanleenden@gmail.com>
2021-08-16 08:41:30 +0200

    fetching data from google api

---
+++ pages/search.js
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import Header from "../components/Header";
+import { API_KEY, CONTEXT_KEY } from "../keys";
+import response from "../response";
+
+const SearchPage = ({ results }) => {
+  const router = useRouter();
+  console.log(results);
+  return (
+    <div>
+      <Head>
+        <title>{router.query.term} - Google Search</title>
+      </Head>
+      <Header />
+      {/* Search Result */}
+    </div>
+  );
+};
+
+export default SearchPage;
+
+export const getServerSideProps = async (context) => {
+  const useDummyData = true;
+  const startIndex = context.query.start || 0;
+
+  const data = useDummyData
+    ? response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
+};
