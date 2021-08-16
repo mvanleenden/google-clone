a80f31c4125a29548b84d9ad384568002bc9ffee
Mayron van Leenden <mvanleenden@gmail.com>
2021-08-16 08:39:46 +0200

    updating the avatar component to accept custom class

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,7 +1,7 @@
-const Avatar = ({ url }) => {
+const Avatar = ({ url, className }) => {
   return (
     <img
-      className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-125"
+      className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-125 ${className}`}
       loading="lazy"
       src={url}
       alt="profile picture"
