2681e54086ebffcb0692408f576aeb9bb6e08e2c
Mayron van Leenden <mvanleenden@gmail.com>
2021-08-16 08:41:08 +0200

    google search keys

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Key - https://developer.google.com/custom-search/v1/using_rest
+export const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_SEARCH_API_KEY;
+
+// Context Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = process.env.NEXT_PUBLIC_GOOGLE_SEARCH_CONTEXT_KEY;
